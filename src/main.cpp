bc4ff7592f8e03a5cbd671a963df88855ebbde92
espositoandrea <esposito_andrea99@hotmail.com>
2019-02-26 23:48:51 +0100

    Added resources' docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 /*! 
  * \mainpage The Doomed Ship: an Adventure
  * \image html Logo/Logo.png width=250px
+ * 
  * \section intro_sec Introduction
  * 
  * This project was started by a group of five students of Computer Science 
  * (the group **FSC**) as part of an exam and it developed into something
  * bigger than it originally was.
  * 
- * \section the_website This website
+ * \section the_website This Website
  * 
  * This website contains the documentation of both the code and the project
  * itself. It has been generated using Doxygen and stylized using the M.CSS's
  * Doxygen template.
+ * 
+ * \section important_pages Important Pages
+ * \ref resources &mdash; A description of all the resources used in this project.
  */
 
 #include <iostream>
